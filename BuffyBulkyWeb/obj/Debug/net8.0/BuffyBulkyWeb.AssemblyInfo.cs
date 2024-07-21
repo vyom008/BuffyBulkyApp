4a6227fc4ceaa2c1e31ba3935d3fb0e4e7f64ac2
Vyom <vyom08@gmail.com>
2024-07-21 13:50:16 +1200

    First commit

--- BuffyBulkyWeb/obj/Debug/net8.0/BuffyBulkyWeb.AssemblyInfo.cs
+++ BuffyBulkyWeb/obj/Debug/net8.0/BuffyBulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuffyBulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34c48518e819e16ef46d4c0c9e9cc5aaf0a50742")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4bd133f4794ed7096182676742cd6ad039fcb1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuffyBulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuffyBulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
