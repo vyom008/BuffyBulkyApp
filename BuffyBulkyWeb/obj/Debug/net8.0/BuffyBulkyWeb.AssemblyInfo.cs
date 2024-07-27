ae725eecfbf337f5a06801bfd5ac4b2a678b2b78
Vyom <vyom08@gmail.com>
2024-07-27 13:14:19 +1200

    Section 4

--- BuffyBulkyWeb/obj/Debug/net8.0/BuffyBulkyWeb.AssemblyInfo.cs
+++ BuffyBulkyWeb/obj/Debug/net8.0/BuffyBulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuffyBulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c26483762125504bbb2f93b585f516ae4e066aef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaf0924ff1fcbc92b6ce256461284261798f279d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuffyBulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuffyBulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
