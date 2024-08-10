b155f8e1e2b7354982841601281678c16e259270
Vyom <vyom08@gmail.com>
2024-08-10 14:15:44 +1200

    Update Product and added Category Name

--- BuffyBulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BuffyBulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -19,7 +19,7 @@ namespace BuffyBulkyWeb.Areas.Admin.Controllers
         }
         public IActionResult Index()
         {
-            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties:"Category").ToList();
 
             return View(objProductList);
         }
@@ -60,6 +60,18 @@ namespace BuffyBulkyWeb.Areas.Admin.Controllers
                     string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
                     string productPath = Path.Combine(wwwRootPath, @"images\product");
 
+                    if (!string.IsNullOrEmpty(productVM.Product.ImageUrl))
+                    {
+                        //delete the old image
+                        var oldOmagePath  = 
+                            Path.Combine(wwwRootPath,productVM.Product.ImageUrl.TrimStart('\\'));
+
+                        if (System.IO.File.Exists(oldOmagePath)) 
+                        {
+                            System.IO.File.Delete(oldOmagePath);                        
+                        }
+                    }
+
                     using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
                     {
                         file.CopyTo(fileStream);
@@ -68,7 +80,15 @@ namespace BuffyBulkyWeb.Areas.Admin.Controllers
                     productVM.Product.ImageUrl = @"\images\product\" + fileName;
                 }
 
-                _unitOfWork.Product.Add(productVM.Product);
+                if(productVM.Product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(productVM.Product);
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(productVM.Product);
+                }
+                
                 _unitOfWork.Save();
                 TempData["success"] = "Product created successfully";
                 return RedirectToAction("Index");
--- Bulky.DataAccess/obj/Debug/net8.0/Bulky.DataAccess.AssemblyInfo.cs
+++ Bulky.DataAccess/obj/Debug/net8.0/Bulky.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53f42b5445d080211a8b26865f31555af50b0876")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75a29ece3badcd99db39f01f942d8a27ced91fcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
