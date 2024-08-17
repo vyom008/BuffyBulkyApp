f727bbe11b19f0aafbbefd569f937e886c8dc715
Vyom <vyom08@gmail.com>
2024-08-17 14:25:06 +1200

    Identity some more updates

--- Bulky.Utility/obj/Debug/net8.0/Bulky.Utility.AssemblyInfo.cs
+++ Bulky.Utility/obj/Debug/net8.0/Bulky.Utility.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulky.Utility")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+944d4f5be4804957829ce2e29b0c098f1c8091aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b84e63a82c24c5c261695afdc17ec88012df6be")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulky.Utility")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulky.Utility")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
